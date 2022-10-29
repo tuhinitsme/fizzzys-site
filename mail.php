f1317314ff6c8e577ab689a95781c67f574dac05
it's me <mehedi.tu@gmail.com>
2022-10-29 15:43:19 +0600

    update al l files
    updated files for site

--- mail.php
+++ mail.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "fauziahossain78@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
